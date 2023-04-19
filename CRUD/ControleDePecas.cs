68f1078cfb780785ded7cde3bcde142fbce0d9cd
Pedro <pedrooliveiradevs@gmail.com>
2023-04-19 10:37:13 -0300

    Testando evendo de Edição

--- CRUD/ControleDePecas.cs
+++ CRUD/ControleDePecas.cs
@@ -1,4 +1,6 @@
-﻿namespace CRUD
+﻿using System.Windows.Forms;
+
+namespace CRUD
 {
     public partial class ControleDePecas : Form
     {   
@@ -35,8 +37,14 @@
 
         private void aoEditar_Click(object sender, EventArgs e)
         {
-            
-            
+
+            var id = int.Parse(dataGridView2.SelectedRows[0].Cells[0].Value.ToString());
+            var peca = listaPecas.Find(x => x.Id == id);
+
+
+
+            CadastroDePecas cadastroDePecas = new CadastroDePecas(null);
+            cadastroDePecas.ShowDialog();
         }
 
         private void ControleDePecas_Load(object sender, EventArgs e)
