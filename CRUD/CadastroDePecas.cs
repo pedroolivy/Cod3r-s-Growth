350673065d86f8eff2f70d7059153872e69e012e
Pedro <pedrooliveiradevs@gmail.com>
2023-04-25 11:20:51 -0300

    Correções nas validações

--- CRUD/CadastroDePecas.cs
+++ CRUD/CadastroDePecas.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace CRUD
 {
@@ -26,27 +28,41 @@ namespace CRUD
 
         private void AoClicarEmSalvar(object sender, EventArgs e)
         {
+            //System.Text.RegularExpressions.           
+
             if (textBox2.Text == string.Empty)
             {
                 MessageBox.Show("campo Nome vazio!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
-            else if (System.Text.RegularExpressions.Regex.IsMatch(textBox4.Text, "[^0-9]") || string.IsNullOrEmpty(textBox4.Text))
+
+            else if (Regex.IsMatch(textBox4.Text, "[^0-9]") || string.IsNullOrEmpty(textBox4.Text))
             {
-                MessageBox.Show("Insira somente números.");
+                MessageBox.Show("Preencha o campo Descrição e insira somente números.");
                 textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
                 return;
             }
+
             else if (textBox6.Text == string.Empty)
             {
                 MessageBox.Show("campo Categoria vazio!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
+
             else if (textBox1.Text == string.Empty)
             {
                 MessageBox.Show("campo Descrição vazio!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
+
+            else if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("campo Descrição vazio!", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            dateTimePicker1.MaxDate = DateTime.Today;
+
 
             var pecaParaAdicionar = new Peca()
             {
@@ -66,5 +82,7 @@ namespace CRUD
         {
             Close();
         }
+
+
     }
 }
