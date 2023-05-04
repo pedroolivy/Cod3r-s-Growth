70e6d34596395f417dab2c4a326273b8836f24c3
Pedro <pedrooliveiradevs@gmail.com>
2023-05-04 09:55:48 -0300

    Criando arquivo de migração e inicando código

---
+++ CRUD/20230504_AdicionaTabelaPecas.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace CRUD
+{
+    [Migration(20230504094800)]
+    public class AdicionarTabelaPecas : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Peca")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Nome").AsString()
+                .WithColumn("Categoria").AsString()
+                .WithColumn("Descricao").AsString()
+                .WithColumn("Estoque").AsString()
+                .WithColumn("DataDeFabricacao").AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Peca");
+        }
+    }
+}
