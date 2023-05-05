406d6e949d9234777fc0409b2b25df733bdbd38c
Pedro <pedrooliveiradevs@gmail.com>
2023-05-05 09:49:47 -0300

    Criado novo repositório para as operações

---
+++ CRUD/Repositorio/RepositorioComBancoSql.cs
@@ -0,0 +1,27 @@
+﻿
+namespace CRUD.Repositorio
+{
+    public class RepositorioComBancoSql : IRepositorio
+    {
+
+        public List<Peca> ObterTodos()
+        {
+        }
+
+        public Peca? ObterPorId(int id)
+        {
+        }
+
+        public void Remover(int id)
+        {
+        }
+
+        public void Adicionar(Peca pecaNova)
+        {
+        }
+
+        public void Editar(int id, Peca pecaEditada)
+        {
+        }
+    }
+}
