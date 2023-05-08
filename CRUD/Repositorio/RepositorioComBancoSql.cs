d15125ddfba19e1ab54423bbac15639e4d4ef57b
Pedro <pedrooliveiradevs@gmail.com>
2023-05-08 12:58:02 -0300

    Metodo de ObterPorId adicionado

--- CRUD/Repositorio/RepositorioComBancoSql.cs
+++ CRUD/Repositorio/RepositorioComBancoSql.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
@@ -42,13 +43,39 @@ namespace CRUD.Repositorio
             
             return lista;
         }   
-        public Peca? ObterPorId(int id) 
+
+        public Peca ObterPorId(int id) 
         {
-            return null;
+
+            SqlConnection conexaoBanco = new SqlConnection(connectionString);
+
+            conexaoBanco.Open();
+
+            SqlCommand comandoDeExecucao = new SqlCommand($"SELECT * FROM Pecas WHERE Id = {id}", conexaoBanco);
+
+            var ler = comandoDeExecucao.ExecuteReader();
+
+            while (ler.Read())
+            {
+                peca = new Peca()
+                {
+                    Id = Convert.ToInt32(ler[0]),
+                    Categoria = ler[1].ToString(),
+                    Nome = ler[2].ToString(),
+                    Descricao = ler[3].ToString(),
+                    Estoque = int.Parse(ler[4].ToString()),
+                    DataDeFabricacao = Convert.ToDateTime(ler[5])
+                };               
+            }
+
+            conexaoBanco.Close();
+
+
+            return peca;
         }
 
         public void Remover(int id) { }
-
+                  
         public void Adicionar(Peca pecaNova) { }
 
         public void Editar(int id, Peca pecaEditada) { }
