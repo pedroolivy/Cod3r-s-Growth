11c98fc5269b5cee2c4ca7d796a1ecc02058fa23
Pedro <pedrooliveiradevs@gmail.com>
2023-05-15 11:27:13 -0300

    Conexão com DB realizada

--- INFRA/Repositorio/RepositorioLinq2Db.cs
+++ INFRA/Repositorio/RepositorioLinq2Db.cs
@@ -1,10 +1,43 @@
-﻿using DOMINIO;
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
+using DOMINIO;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.SqlServer;
 
 namespace INFRA.Repositorio
 {
     internal class RepositorioLinq2Db : IRepositorio
     {
-        
+        public static DataConnection ConexaoLin2Db()
+        {
+            var DataConnection = ConfigurationManager.ConnectionStrings["ConexaoBD"].ConnectionString;
+            var conexao = SqlServerTools.CreateDataConnection(DataConnection);
+            return conexao;
+        }
 
+        public void Adicionar(Peca pecaNova)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(int id, Peca pecaEditada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Peca? ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Peca> ObterTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
