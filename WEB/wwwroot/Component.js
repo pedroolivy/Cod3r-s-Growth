2b834db6e194e7d197207e9078ae00dde9016cb1
Pedro <pedrooliveiradevs@gmail.com>
2023-05-26 08:57:45 -0300

    Códigos básicos implementados

--- WEB/wwwroot/Component.js
+++ WEB/wwwroot/Component.js
@@ -11,31 +11,6 @@ sap.ui.define([
 		},
 		init: function () {
 			UIComponent.prototype.init.apply(this, arguments);
-
-			var oData = {
-				recipient: {
-					name: "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.setModel(oModel);
-
-			var oDeviceModel = new JSONModel(Device);
-			oDeviceModel.setDefaultBindingMode("OneWay");
-			this.setModel(oDeviceModel, "device");
-
-			this.getRouter().initialize();
-		},
-		getContentDensityClass : function () {
-			if (!this._sContentDensityClass) {
-				if (!Device.support.touch) {
-					this._sContentDensityClass = "sapUiSizeCompact";
-				} else {
-					this._sContentDensityClass = "sapUiSizeCozy";
-				}
-			}
-			return this._sContentDensityClass;
 		}
-
 	});
 });
