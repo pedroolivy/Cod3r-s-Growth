309fc8d2913a459cf75fce8af936caac07be6cb4
Pedro <pedrooliveiradevs@gmail.com>
2023-05-26 11:46:21 -0300

    Classe da API adicionada no meu projeto WEB

--- WEB/wwwroot/Component.js
+++ WEB/wwwroot/Component.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"sap/ui/Device"
 ], function (UIComponent, JSONModel, Device) {
 	"use strict";
-	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+	return UIComponent.extend("sap.ui.demo.CrudPecas.Component", {
 		metadata: {
 			interfaces: ["sap.ui.core.IAsyncContentCreation"],
 			manifest: "json"
