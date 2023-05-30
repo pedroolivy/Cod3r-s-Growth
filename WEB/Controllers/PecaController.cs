911c228804a90f343e7a74e7d1cd1d39362d804c
Pedro <pedrooliveiradevs@gmail.com>
2023-05-30 16:12:51 -0300

    Lógica de Obter e ObterPorId adicionada

--- WEB/Controllers/PecaController.cs
+++ WEB/Controllers/PecaController.cs
@@ -1,30 +1,31 @@
 ﻿using DOMINIO;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace WEB.Controllers
 {
     [ApiController]
-    [Route("pecas")]
+    [Route("api/v1/peca")]
     public class PecaController : ControllerBase
     {
         private readonly IRepositorio _repositorio;
 
-        public PecaController(IRepositorio _repositorio)
+        public PecaController(IRepositorio repositorio)
         {
-            this._repositorio = _repositorio;
+            _repositorio = repositorio;
         }
 
         [HttpGet]
         public IActionResult ObterTodos()
         {
-            var peca = _repositorio.ObterTodos();
+            var listaDePecas = _repositorio.ObterTodos().ToList();
 
-            if (peca == null)
+            if (listaDePecas == null)
             {
                 return NotFound();
             }
 
-            return Ok(peca);
+            return Ok(listaDePecas);
         }
 
         [HttpGet("{id}")]
@@ -36,23 +37,31 @@ namespace WEB.Controllers
             {
                 return NotFound();
             }
-
             return Ok(peca);
         }
 
         [HttpPost("{pecaNova}")]
         public IActionResult Adicionar(Peca pecaNova)
         {
-            var peca = _repositorio;
-            peca.Adicionar(pecaNova);
-            pecaNova.Id = Singleton.ObterProximoId();
+            _repositorio.Adicionar(pecaNova);
+
+            if (pecaNova == null)
+            {
+                return BadRequest();
+            }
+
+            return Created($"Peca/{pecaNova}", pecaNova);
         }
 
         [HttpPut("{id, pecaEditada}")] 
-        public IActionResult Editar(Guid id, Peca pecaEditada)
-
+        public IActionResult Editar(int id, [FromBody] Peca pecaEditada)
         {
-            return null;
+            _repositorio.Editar(id, pecaEditada);
+
+            if (pecaNova == null)
+            {
+                return BadRequest();
+            }
         }
 
         [HttpDelete("{id}")]
