902f6dc23e7f103b3bcd12614b515431a77c852b
Pedro <pedrooliveiradevs@gmail.com>
2023-06-16 08:17:39 -0300

    Tela de detalhe recebe e mostra meu objeto

--- WEB/wwwroot/controller/Detalhe.controller.js
+++ WEB/wwwroot/controller/Detalhe.controller.js
@@ -11,15 +11,17 @@ sap.ui.define([
 
 		onInit: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
-			oRouter.getRoute("Detalhe").attachPatternMatched(this._aoCoincidirRota, this);
+			oRouter.getRoute(detalheDaPeca).attachPatternMatched(this._aoCoincidirRota, this);
 		},
-		_aoCoincidirRota: function () {
-			fetch(api)
+		_aoCoincidirRota: function (oEvent) {
+			var teste = oEvent.getParameter("arguments").id
+			fetch(`${api}/${teste}`)
             .then(response => response.json())
             .then(json => {
                 var oModel = new JSONModel(json);
                 this.getView().setModel(oModel, detalheDaPeca);
         	})
+			
         },
 		onNavBack: function () {
 			var oRouter = this.getOwnerComponent().getRouter();
